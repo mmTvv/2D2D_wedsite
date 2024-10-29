fc7b6c08937a51697d4631491f291feef0c0c564
mmTvv <iam@matveev.website>
2024-10-29 08:53:07 +0300

    demo ver

--- app.py
+++ app.py
@@ -38,5 +38,9 @@ def donate():
 def help_page():
     return render_template('help.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=1000)
--- static/js/main.js
+++ static/js/main.js
@@ -24,3 +24,34 @@ themeToggle.addEventListener('click', () => {
         localStorage.setItem('theme', 'dark');
     }
 });
+
+document.querySelector('.js-transition').addEventListener('click', toggle);
+
+function toggle() {
+   var container = document.querySelector('.transition-container');
+  
+  // manage clases
+  var containerClasses = container.classList;
+  containerClasses.toggle('js-show-pw');
+  containerClasses.toggle('eye-open');
+  containerClasses.toggle('eye-close');
+  
+  // update input
+  var input = document.querySelector('.transition-input');
+  
+  if (containerClasses.contains('js-show-pw')) {
+    // use time = animation duration
+    var delay = getComputedStyle(container).getPropertyValue('--duration');
+    delay = parseInt(delay);
+    
+    setTimeout(function() {
+      input.classList.add('show-input');
+      // update input type
+      input.type="text";
+    }, delay);
+  } else {
+    // update input type
+    input.type="password";
+    input.classList.remove('show-input');
+  }
+}
